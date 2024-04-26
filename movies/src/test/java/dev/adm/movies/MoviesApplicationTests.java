6944bbbc65d7261e8e4aef581804178714ce13d7
Adwaithmadhusudhanan <140256348+Adwaithmadhusudhanan@users.noreply.github.com>
2024-04-26 12:36:57 +0530

    Add files via upload

---
+++ movies/src/test/java/dev/adm/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adm.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
